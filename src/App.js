d83465d27b415ad4e8b8618cb02a8ccf70b8c801
unknown <root@CSMBHUL617.corp.csmpl.com>
2022-05-18 10:23:06 +0530

    Chinmayee Task Page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Dashboard from './pages/dashboard/Dashboard';
 import Chinmayee from './pages/Chinmayee/Chinmayee';
+import { Card } from 'react-bootstrap';
 function App1() {
-  const [cPage, setCPage] = useState("home")
-  //console.log(cPage)
+  const [cPage, setCPage] = useState("home");
+
+  const [feedbackData, setFeedbackData] = useState({});
+  const [distRes, setDistRes] = useState([]);
+  const getFeedBackData= async()=>{
+
+    // const getFeedback = await fetch("http://164.164.122.169:8090/state-dashboard/api/BskyData/getFeedback/97d7f7d0-2c6e-4f80-bce2-0d8b5cfaab5b");
+    // console.log("getFeedback::::",getFeedback);
+
+    await fetch("http://164.164.122.169:8090/state-dashboard/api/BskyData/getFeedback/97d7f7d0-2c6e-4f80-bce2-0d8b5cfaab5b")
+    .then(response =>  
+      response.json()
+    )
+    .then(data => {
+      console.log("data::::",data);
+      setFeedbackData(data);
+  });
+  console.log("getFeedback1:::",feedbackData.status)
+    
+  }
+  useEffect(()=>{
+    getFeedBackData();
+  },[]);
+
+  useEffect(()=>{
+    if(feedbackData.distRes){
+      setDistRes(feedbackData.distRes);
+    }
+  },[feedbackData]);
+
   return (
     <>
       <div>
         <Header setCPage={setCPage} />
       </div>
+      <h1>{feedbackData.status}</h1>
+      {distRes.map(item=>{
+        return(<Card>
+        <h2>{item.vchName}</h2>
+        </Card>)
+      })}
       <Chinmayee/>
       <Dashboard />
       {cPage !== "" && (cPage === 'home' || cPage === "Home") && (
